d180e31ca202ab5c64b6c0cd3e1c3520e659639d
harshal-starkenn <127827603+harshal-starkenn@users.noreply.github.com>
2024-03-18 18:34:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 const { client } = require("./mqtt");
 const cors = require("cors");
 const { tripsSocket } = require("./socketFunc");
+const pool = require("./db.js");
 
 const app = express();
 app.use(express.json());
@@ -14,47 +15,69 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 // send data from database through this
-tripsSocket(io);
+// tripsSocket(io);
 
-client.on("connect", () => {
-  console.log("Connected to MQTT!");
-  //starkennInv3/${row.device_id}/data
-  client.subscribe(`#`, (err) => {
-    if (err) {
-      console.error("Error in subscribing to topic:", err);
-    } else {
-      console.log("Subscribed to the topic");
+const mqttTrigger = () => {
+  client.on("connect", () => {
+    console.log("Connected to MQTT!");
+    //starkennInv3/${row.device_id}/data
+    client.subscribe(`#`, (err) => {
+      if (err) {
+        console.error("Error in subscribing to topic:", err);
+      } else {
+        console.log("Subscribed to the topic");
+      }
+    });
+  });
+
+  client.on("message", (topic, message) => {
+    try {
+      if (message) {
+        let data = message.toString();
+        // console.table([data, topic]);
+        const parseData = JSON.parse(data);
+        // console.log("MQTT device ID", parseData.device_id);
+        axios
+          .post(
+            "https://5n6zcgmq7a.execute-api.ap-south-1.amazonaws.com/dev/validateJson",
+            { msg: data }
+          )
+          .then((response) => {
+            // console.log("API Response:", response.data.message);
+
+            if (response) {
+              tripsSocket(io, parseData.device_id);
+            } else {
+              console.log(
+                "Error in emmiting data to socket!::::",
+                err.response.data
+              );
+            }
+          })
+          .catch((err) => {
+            console.error("Error::::::", err.response.data.message);
+          });
+      } else {
+        console.log("Received null message from MQTT");
+      }
+    } catch (err) {
+      console.error("Error processing MQTT message:", err);
     }
   });
-});
+};
 
-client.on("message", (topic, message) => {
-  try {
-    if (message) {
-      let data = message.toString();
-      console.table([data, topic]);
-      // https://5n6zcgmq7a.execute-api.ap-south-1.amazonaws.com/dev/validateJson
-      axios
-        .post(
-          "https://5n6zcgmq7a.execute-api.ap-south-1.amazonaws.com/dev/validateJson",
-          { msg: data }
-        )
-        .then((response) => {
-          console.log("API Response:", response.data.message);
-        })
-        .catch((err) => {
-          console.error("Error::::::", err.response.data);
-        });
-    } else {
-      console.log("Received null message from MQTT");
-    }
-  } catch (err) {
-    console.error("Error processing MQTT message:", err);
-  }
-});
+mqttTrigger();
 
-app.post("/mqtt", (req, res) => {
-  res.status(200).send("OK");
+// app.post("/mqtt", (req, res) => {
+//   res.status(200).send("OK");
+// });
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 const PORT = process.env.PORT || 8080;
--- socketFunc.js
+++ socketFunc.js
@@ -1,52 +1,96 @@
 const pool = require("./db.js");
 
-const tripsSocket = async (io) => {
+const tripsSocket = async (io, deviceID) => {
   let connection = null;
 
   try {
-    // socket connection
-    io.on("connection", async (socket) => {
-      let emitAndFetchFunction = async () => {
-        try {
-          const Query = `SELECT json_arrayagg(json_object('trip_id', TS.trip_id, 'startTime', TS.trip_start_time, 'device_id', TS.device_id, 'location_data', (SELECT json_arrayagg(json_object('latitude', td.lat, 'longitude', td.lng, 'speed', td.spd)) FROM TripData td WHERE td.trip_id = TS.trip_id ORDER BY td.timestamp ASC))) AS tripDetails FROM TripSummary TS WHERE TS.trip_status = 0`;
+    let emitAndFetchFunction = async () => {
+      try {
+        const Query = `SELECT 
+        json_arrayagg(
+            json_object(
+                'trip_id', TS.trip_id, 
+                'startTime', TS.trip_start_time, 
+                'device_id', TS.device_id, 
+                'location_data', (
+                    SELECT json_arrayagg(
+                        json_object(
+                            'latitude', td.lat, 
+                            'longitude', td.lng, 
+                            'speed', td.spd
+                        )
+                    ) 
+                    FROM TripData td 
+                    WHERE td.trip_id = TS.trip_id 
+                    ORDER BY td.timestamp ASC
+                ),
+                'alerts', (
+                    SELECT json_arrayagg(
+                        json_object(
+                            'event', TD.event,
+                            'message',TD.message,
+                            'device_id',TD.device_id,
+                            'timestamp',TD.timestamp,
+                            'latitude',TD.lat,
+                            'longitude',TD.lng,
+                            'spd',TD.spd,
+                            'vehicle_id',TD.vehicle_id
+                        )
+                    ) 
+                    FROM TripData TD 
+                    WHERE TD.trip_id = TS.trip_id 
+                    AND TD.event NOT IN ('IGS', 'LOC')
+                )
+            )
+        ) AS tripDetails 
+         FROM 
+        TripSummary TS 
+        WHERE 
+        TS.trip_status = 0`;
 
-          connection = await pool.getConnection();
+        connection = await pool.getConnection();
 
-          const [results] = await connection.execute(Query);
+        const [results] = await connection.execute(Query);
 
-          if (results && results.length && results[0].tripDetails) {
-            let allTripIds = [];
-            // Filter all trip IDs
-            if (results[0].tripDetails.length) {
-              results[0].tripDetails.map((item) => {
-                allTripIds.push(item.trip_id);
-              });
-
-              // Emit to all the trip ID
-              allTripIds.map((item) => {
-                io.emit(
-                  item,
-                  results[0].tripDetails.filter((it) => it.trip_id === item)[0]
-                );
-              });
-            }
-          } else {
-            console.log("There are no ongoing trips or an error occurred!");
-          }
-        } catch (err) {
-          console.log("error in sending data to socket::", err);
-        } finally {
-          if (connection) {
-            connection.release();
-          }
+        if (connection) {
+          connection.release();
         }
-      };
 
-      //Call function to emit data from database
-      setInterval(() => {
-        emitAndFetchFunction();
-      }, 5000);
-    });
+        if (results && results.length && results[0].tripDetails) {
+          let allTripIds = [];
+          // Filter all trip IDs
+          if (results[0].tripDetails.length) {
+            results[0].tripDetails.map((item) => {
+              if (item.device_id === deviceID) {
+                // console.log("coming device", deviceID);
+                allTripIds.push(item.trip_id);
+              }
+            });
+
+            // Emit to all the trip ID
+            allTripIds.map((item) => {
+              // console.log("tripDetails:::", results[0].tripDetails);
+              // if(results[0].tripDetails.device_id)
+              io.emit(
+                item,
+                results[0].tripDetails.filter((it) => it.trip_id === item)[0]
+              );
+            });
+          }
+        } else {
+          console.log("There are no ongoing trips or an error occurred!");
+        }
+      } catch (err) {
+        console.log("error in sending data to socket::", err);
+      } finally {
+        if (connection) {
+          connection.release();
+        }
+      }
+    };
+
+    //Call function to emit data from database
+    emitAndFetchFunction();
   } catch (err) {
     console.log("error::", err);
   }
